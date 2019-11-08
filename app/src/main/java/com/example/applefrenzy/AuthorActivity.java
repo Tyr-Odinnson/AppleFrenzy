3aa9024d4d24b9c70c017eb6758ed79c7132fbdc
Tyr-Odinnson <onizenbuddha@gmail.com>
2019-11-08 23:37:16 +1300

    Needs more content, but mostly done.

--- app/src/main/java/com/example/applefrenzy/AuthorActivity.java
+++ app/src/main/java/com/example/applefrenzy/AuthorActivity.java
@@ -2,13 +2,28 @@ package com.example.applefrenzy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthorActivity extends AppCompatActivity {
+
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent(AuthorActivity.this, MainActivity.class);
+                startActivity(returnIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/applefrenzy/StoryActivity.java
+++ app/src/main/java/com/example/applefrenzy/StoryActivity.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HowToActivity extends AppCompatActivity {
+public class StoryActivity extends AppCompatActivity {
 
     private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_how_to);
+        setContentView(R.layout.activity_story);
 
         backButton = findViewById(R.id.backButton);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent returnIntent = new Intent(HowToActivity.this, MainActivity.class);
+                Intent returnIntent = new Intent(StoryActivity.this, MainActivity.class);
                 startActivity(returnIntent);
             }
         });
