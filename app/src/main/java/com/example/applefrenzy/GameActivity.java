96a67d34d78d3b20dd35005ca2b1510e041e47db
Tyr-Odinnson <50427951+Tyr-Odinnson@users.noreply.github.com>
2019-11-08 15:43:54 +1300

    Collision working, as well as scoring.

--- app/src/main/java/com/example/applefrenzy/GameActivity.java
+++ app/src/main/java/com/example/applefrenzy/GameActivity.java
@@ -7,34 +7,75 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class GameActivity extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener ,Runnable{
+public class GameActivity extends AppCompatActivity implements View.OnTouchListener, GestureDetector.OnGestureListener{
+    RelativeLayout relativeLayout;
     ConstraintLayout layout;
     AnimationDrawable drawable;
+
+    public static final int swipeThreshold = 100;
+    public static final int swipeVelocityThreshold = 500;
     GestureDetector gestureDetector;
-    private ImageView apple;
-    private ImageView basket;
-    TextView scoreval,missval;
-    int layoutw,layouth;
-    int speedy =10;
+    float GestureStartX, GestureEndX, GestureStartY, GestureEndY, XMovement,YMovement;
+    int gestureType;
+
+    private TextView scoreLabel;
+    private TextView missingLabel;
+
+    private ImageView imgApple;
+    private ImageView imgGround;
+    private ImageView imgBasket;
+    private ImageView pauseButton;
+    private PopupWindow popupWindow;
+    private LayoutInflater layoutInflater;
+
+    int applesCounter =0;
+    int missedCounter =0;
+    boolean applesdropped = false;
     int applexpos = 10;
-    int score =0, missed =0;
     Random r;
     boolean appleXstate = false,appleYstate = false;
     boolean score_state = false, miss_state = false;
 
-//    Rect r1 = new Rect(100,100,100,100);
-//    Rect r2 = new Rect(95,95,95,95);
+    Rect apple = new Rect();
+    Rect ground = new Rect();
+    Rect rectBasket = new Rect();
+
+    private float appleX;
+    private float appleY;
+    private float groundX;
+    private float groundY;
+    private float basketX;
+    private float basketY;
+    int score = 0;
+    int missed = 0;
+
+    private int layoutWidth;
+    private int layoutHeight;
+
+    private Handler handler = new Handler();
+    private Timer timer = new Timer();
+  //  private SoundPlayer sound;
 
 
     @Override
@@ -42,55 +83,156 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-
-
         layout = findViewById(R.id.myLayout);
-        scoreval = findViewById(R.id.scoreval);
-        missval = findViewById(R.id.missingval);
-        scoreval.setText(Integer.toString(score));
-        missval.setText(Integer.toString(missed));
         gestureDetector = new GestureDetector(this);
         drawable = (AnimationDrawable) layout.getBackground();
         drawable.setEnterFadeDuration(1000);
         drawable.setExitFadeDuration(1000);
         drawable.start();
 
-         Display display = getWindowManager().getDefaultDisplay();
-         Point windsize = new Point();
+       // sound = new SoundPlayer(this);
+        imgApple = findViewById(R.id.appleDrop);
+        imgGround = findViewById(R.id.ground);
+        imgBasket = findViewById(R.id.basket);
+        scoreLabel = (TextView) findViewById(R.id.scoretxt);
+        missingLabel = (TextView) findViewById(R.id.missingapple);
+        gestureDetector = new GestureDetector(this);
+        r = new Random();
 
-          display.getSize(windsize);
-          layoutw = windsize.x;
-          layouth = windsize.y;
+//        pauseButton = findViewById(R.id.pause);
+//        relativeLayout = (RelativeLayout) findViewById(R.id.pause_menu);
+//        pauseButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+//                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.pause_menu,null);
+//                popupWindow = new PopupWindow(container, 350, 350, true);
+//                popupWindow.showAtLocation(relativeLayout,Gravity.NO_GRAVITY,500, 500);
+//
+//                container.setOnTouchListener(new View.OnTouchListener() {
+//                    @Override
+//                    public boolean onTouch(View view, MotionEvent motionEvent) {
+//                        popupWindow.dismiss();
+//                        return true;
+//                    }
+//                });
+//            }
+//        });
 
-          r = new Random();
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        layoutWidth = size.x;
+        layoutHeight = size.y;
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        change_apple_pos();
+                        collision();
+                        changePos();
+                        speed();
+                    }
+                });
+            }
+        }, 0, 10);
 
-        apple = findViewById(R.id.appleDrop);
-        basket = findViewById(R.id.basket);
+        scoreLabel.setText("Score : 0");
+        missingLabel.setText("Missed : 0");
+    }
 
+    public void collision () {
+        imgApple.getHitRect(apple);
+        imgGround.getHitRect(ground);
+        imgBasket.getHitRect(rectBasket);
 
+        if (Rect.intersects(apple,rectBasket)){
+            appleX = (float)Math.floor(Math.random() * (layoutWidth -  imgApple.getWidth()));
+            appleY = -100.0f;
+            imgApple.setX(appleX);
+            imgApple.setY(appleY);
+            score+=27;
+            scoreLabel.setText("Score : " + score);
+            //    sound.playHitSound();
+        }
 
-//        apple.getHitRect(r1);
-//        basket.getHitRect(r2);
+        if (Rect.intersects(apple,ground)){
+            appleX = (float)Math.floor(Math.random() * (layoutHeight -  imgGround.getWidth()));
+            appleY = -100.0f;
+            imgApple.setX(groundX);
+            imgApple.setY(groundY);
+            //    sound.playHitSound();
 
-        basket.setOnTouchListener(this);
+        }
+    }
 
-        basket.setMaxWidth(95);
+    public void changePos() {
+        appleY += 10;
+        if (imgApple.getY() >= layoutHeight -100) {
+            applexpos = r.nextInt(layoutWidth - imgApple.getWidth());
+            appleY = -100;
+            applesCounter++;
+        }
+        imgApple.setX(appleX);
+        imgApple.setY(appleY);
 
-        new Thread(this).start();
-
-
-
-
-//        Toast.makeText(getApplication(),"rect size  "+r1.width(),Toast.LENGTH_LONG).show();
 
     }
+
+
+    public void change_apple_pos() {
+        imgApple.getHitRect(apple);
+        imgGround.getHitRect(ground);
+        imgBasket.getHitRect(rectBasket);
+        if (Rect.intersects(apple, ground)) {
+            appleX = (float)r.nextInt(layoutWidth - imgApple.getWidth());
+            appleY = -100.0f;
+            imgApple.setX(appleX);
+            imgApple.setY(appleY);
+            missed+=1;
+            missingLabel.setText("Missed : " + missed);
+            Log.d(" change apple position","change apple position");
+        }
+    }
+
+    public void speed(){
+        basketX = imgBasket.getX();
+        switch (gestureType){
+            case 1:
+
+                if (basketX <= layoutWidth - imgBasket.getWidth()) {
+                    basketX = basketX +10;
+                    imgBasket.setX(basketX);
+                }
+                else
+                {
+                    appleYstate = true;
+                }
+
+
+                break;
+
+            case 2:
+                if (basketX >=0) {
+                    basketX = basketX -10;
+                    imgBasket.setX(basketX);
+                }
+
+
+                break;
+        }
+
+    }
+
 
     @Override
-    protected void onStart() {
-        super.onStart();
-
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
     }
-
 
     @Override
     public boolean onDown(MotionEvent motionEvent) {
@@ -109,13 +251,8 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
 
     @Override
     public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-
-//        if(motionEvent1.getX()+basket.getWidth() <= layoutw )
-//            basket.animate().translationXBy(motionEvent1.getX()).setDuration(1000);
-//        if(basket.getX() >= layoutw)
-//        {
-//            basket.animate().translationXBy(-100).setDuration(1000);
-//        }
+//                if(motionEvent1.getX() <= layout.getWidth() )
+//            imgBasket.animate().translationXBy(motionEvent1.getX()).setDuration(1000);
 //        Log.d("Test"," "+ motionEvent1.getX());
         return true;
     }
@@ -126,109 +263,41 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
     @Override
-    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
 
+        GestureStartX = e1.getX();
+        GestureEndX = e2.getX();
+        GestureStartY = e1.getY();
+        GestureEndY = e2.getY();
+        XMovement = GestureEndX - GestureStartX;
+        YMovement = GestureEndY - GestureStartY;
 
-//            basket.animate().translationXBy(motionEvent1.getX()).setDuration(1000);
-//        Log.d("Test"," "+ motionEvent1.getX());
+        if (Math.abs(XMovement) > Math.abs(YMovement)){
+            if (Math.abs(XMovement) > swipeThreshold && Math.abs(velocityX) > swipeVelocityThreshold) {
+                if (XMovement > 0){
+                    Log.d("Testing", "Swipe Right");
+                    gestureType = 1;
+                }else {
+                    Log.d("Testing", "Swipe Left");
+                    gestureType = 2;
+                }
+
+            }
+
+        }
         return true;
     }
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-
-        gestureDetector.onTouchEvent(motionEvent);
-
-        if(motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-            if (motionEvent.getX() + basket.getWidth() <= layoutw)
-               basket.animate().translationXBy(motionEvent.getX()).setDuration(1000);
-             //   basket.animate().translationX(motionEvent.getX()).setDuration(1000);
-
-            if (basket.getX() >= layoutw) {
-               basket.setX(100);
-               // basket.animate().translationX(basket.getWidth()).setDuration(1000);
-            }
-          collision();
-//            if ( apple.getY() >= basket.getY() && apple.getY() + apple.getHeight() >= basket.getY() + basket.getHeight()){
-//                score++;
-//                scoreval.setText(Integer.toString(score));
-//                Log.d("Test","Intersects"+ apple.getWidth()+" basket width "+basket.getWidth());
-//            }
-        }
+//         Log.d("testing", "onTouch");
+//        gestureDetector.onTouchEvent(motionEvent);
+//
+//        if(motionEvent.getAction() == MotionEvent.ACTION_MOVE)
+//        {
+//
+//            imgBasket.animate().translationXBy(motionEvent.getX()).setDuration(100);
+//
+//        }
         return true;
     }
-
-    @Override
-    public void run() {
-
-
-
-        while (true) {
-            try {
-
-                Thread.sleep(100);
-
-                if (apple.getY()+apple.getHeight() <= layouth) {
-                    speedy += 20;
-                    apple.setY(speedy);
-                    collision();
-                   }
-                else
-                {
-                    appleYstate = true;
-                    speedy = 10;
-                    updatex();
-               }
-                miss_apple();
-              //  Log.d("Test", "run called " + speedy);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
-        }
-
-
-    }
-
-
-    public void updatex()
-    {
-        if(appleYstate)
-        {
-            applexpos = r.nextInt(layoutw-apple.getWidth());
-            apple.setY(10);
-            apple.setX(applexpos);
-        }
-        appleYstate = false;
-    }
-
-    public void collision(){
-
-//        if ( apple.getX() >= basket.getX() && apple.getX() + apple.getWidth() <= basket.getX() + basket.getWidth()){
-//             score++;
-//             scoreval.setText(Integer.toString(score));
-//            Log.d("Test","Intersects");
-//        }
-
-        if ( apple.getY() >= basket.getY() && apple.getY() + apple.getHeight() >= basket.getY() && apple.getX() >= basket.getX() && apple.getX() + apple.getWidth() >= basket.getX()){
-            score++;
-            scoreval.setText(Integer.toString(score));
-            score_state= true;
-            miss_state = false;
-            Log.d("Test","Intersects"+ apple.getWidth()+" basket width "+basket.getWidth());
-        }
-
-    }
-
-public void miss_apple()
-{
-    if(miss_state)
-    {
-        missed++;
-        missval.setText(Integer.toString(missed));
-        miss_state = false;
-    }
-
-}
-
 }
